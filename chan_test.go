b948c3b790f2fef9ac940333ae900e99d6e8b76c
Anton Konovalov <anton@aludio.com>
2018-05-14 11:59:50 +0300

    add grpc test

--- chan_test.go
+++ chan_test.go
@@ -6,9 +6,12 @@ import (
 	"net/http"
 	"testing"
 	"time"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-func TestChanConn_Read(t *testing.T) {
+func TestChanConnHttp(t *testing.T) {
     lis, err := ListenChannel(context.Background(),"my-service:0")
     if err != nil {
     	t.Fatalf("unexpected error  - %s",err)
@@ -48,5 +51,57 @@ func TestChanConn_Read(t *testing.T) {
 	}
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("unexpected status code %d", resp.StatusCode )
+	}
+}
+
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func TestChanConnGrpc(t *testing.T) {
+	lis, err := ListenChannel(context.Background(),"my-service:0")
+	if err != nil {
+		t.Fatalf("unexpected error  - %s",err)
+	}
+	defer lis.Close()
+
+	srv := grpc.NewServer()
+	pb.RegisterGreeterServer(srv,new(server))
+
+
+	start := make(chan struct{})
+	go func () {
+		close(start)
+		srv.Serve(lis)
+	}()
+
+	<-start
+
+	conn, err := grpc.DialContext(context.Background(),"my-service:0",
+		grpc.WithInsecure(),
+		grpc.WithDialer(
+		func (addr string, _ time.Duration) (net.Conn,error) {
+			return DialChannel(context.Background(),"channel",addr)
+		}),
+	)
+
+	if err != nil {
+		t.Errorf("unexpected error %s",err)
+	}
+
+	resp , err := pb.NewGreeterClient(conn).SayHello(context.Background(),&pb.HelloRequest{
+		Name:"Aloxa",
+	})
+	if err != nil {
+		t.Errorf("unexpected error %s",err)
+	} else {
+		if resp.Message != "Hello Aloxa" {
+			t.Errorf("messaage resp 'Hello Aloxa' != %s", resp.Message)
+		}
 	}
 }
